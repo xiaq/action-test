461fb7abd1313d9584619a09266ccbf88516a53b
Qi Xiao <xiaqqaix@gmail.com>
2022-06-16 21:12:20 +0100

    Fix escape

--- main.mjs
+++ main.mjs
@@ -11,11 +11,11 @@ async function main() {
     if (platform === 'windows') {
         await run('pwsh', '-c',
             `
-            New-Item -ItemType Directory C:\elvish
-            Write-Output C:\elvish >> $Env:GITHUB_PATH
-            Set-Location C:\elvish
+            New-Item -ItemType Directory C:\\elvish
+            Write-Output C:\\elvish >> $Env:GITHUB_PATH
+            Set-Location C:\\elvish
             Invoke-RestMethod -Uri '${urlBase}.zip' -OutFile elvish.zip
-            Expand-Archive .\elvish.zip -DestinationPath .
+            Expand-Archive elvish.zip -DestinationPath .
             Remove-Item elvish.zip
             New-Item -ItemType SymbolicLink -Path elvish elvish-${version}
             `);
