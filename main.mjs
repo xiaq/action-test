d5b4bc0e2ee0aa4cbe43868054b7d284f2c71b8f
Qi Xiao <xiaqqaix@gmail.com>
2022-06-16 20:08:47 +0100

    Wait script

--- main.mjs
+++ main.mjs
@@ -1,8 +1,19 @@
 import { exec } from 'node:child_process';
 
 async function main() {
-    process.chdir('/usr/local/bin')
-    exec('curl -o- https://dl.elv.sh/linux-amd64/elvish-v0.18.0.tar.gz | tar x')
+    process.chdir('/usr/local/bin');
+    const p = exec(
+        'curl -o- https://dl.elv.sh/linux-amd64/elvish-v0.18.0.tar.gz | tar x',
+        { stdio: 'inherit' });
+    await new Promise((resolve, reject) => {
+        p.on('exit', (code) => {
+            if (code === 0) {
+                res();
+            } else {
+                reject(new Error(`Script exited with ${code}`));
+            }
+        });
+    });
 }
 
 await main()
