027beb99ae84609d18ecf85af08c041fb0126b80
Qi Xiao <xiaqqaix@gmail.com>
2022-06-16 20:26:01 +0100

    Download Elvish

--- main.mjs
+++ main.mjs
@@ -1,19 +1,32 @@
-import { exec } from 'node:child_process';
+import { spawn } from 'node:child_process';
+import { symlink } from 'node:fs/promises';
+
+await main()
 
 async function main() {
-    process.chdir('/usr/local/bin');
-    const p = exec(
-        'curl -o- https://dl.elv.sh/linux-amd64/elvish-v0.18.0.tar.gz | tar x',
-        { stdio: 'inherit' });
-    await new Promise((resolve, reject) => {
-        p.on('exit', (code) => {
+    // process.chdir('/usr/local/bin');
+    await run('sh', '-c',
+        'curl -o- https://dl.elv.sh/linux-amd64/elvish-v0.18.0.tar.gz | tar x');
+    await symlink('elvish-v0.18.0', 'elvish');
+}
+
+function run(cmd, ...args) {
+    const p = spawn(cmd, args, {stdio: 'inherit'});
+    return new Promise((resolve, reject) => {
+        let errored = false;
+        p.on('error', (err) => {
+            errored = true;
+            reject(err);
+        });
+        p.on('exit', (code, signal) => {
+            if (errored) {
+                return;
+            }
             if (code === 0) {
-                res();
+                resolve();
             } else {
-                reject(new Error(`Script exited with ${code}`));
+                reject(new Error(`Exited with ${code} (signal: ${signal})`));
             }
         });
     });
 }
-
-await main()
