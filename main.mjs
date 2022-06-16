554881ae79f4ad42ea721a1b9583621c1fa97467
Qi Xiao <xiaqqaix@gmail.com>
2022-06-16 20:30:39 +0100

    chmod +x

--- main.mjs
+++ main.mjs
@@ -6,7 +6,7 @@ await main()
 async function main() {
     // process.chdir('/usr/local/bin');
     await run('sh', '-c',
-        'curl -o- https://dl.elv.sh/linux-amd64/elvish-v0.18.0.tar.gz | tar xz');
+        'curl -o- https://dl.elv.sh/linux-amd64/elvish-v0.18.0.tar.gz | tar xz; chmod +x elvish-v0.18.0');
     await symlink('elvish-v0.18.0', 'elvish');
 }
 
